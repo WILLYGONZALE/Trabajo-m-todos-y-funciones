a0936a65808796cc0b89068d280f17ee4afc4a41
Willian Gonzalez <willianstebangonzalezcortes@gmail.com>
2024-08-07 00:25:43 -0500

    se agrega el trabajo de polimorfismo y clases y interfases

---
+++ clases y interfases willian steban/clases y interfases willian steban/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+public interface ILavadora
+{
+    void Centrifugar();
+    void Lavar();
+    void Enjuagar();
+    void Remojar();
+}
+public abstract class LavadoraBase : ILavadora
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Año { get; set; }
+
+    public LavadoraBase(string marca, string modelo, int año)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        Año = año;
+    }
+
+    public virtual void Centrifugar()
+    {
+        Console.WriteLine("La lavadora esta Centrifungando.");
+    }
+
+    public virtual void Lavar()
+    {
+        Console.WriteLine("La lavadora esta lavando.");
+    }
+
+    public virtual void Enjuagar()
+    {
+        Console.WriteLine("La lavadora esta enjugando.");
+    }
+
+    public virtual void Remojar()
+    {
+        Console.WriteLine("La lavadora esta remojando.");
+    }
+}
+public class Lavadora : LavadoraBase
+{
+    public int Tamaño { get; set; }
+
+    public Lavadora(string marca, string modelo, int año, int Tamaño)
+        : base(marca, modelo, año)
+    {
+        Tamaño = Tamaño;
+    }
+
+    // Métodos específicos para La Lavadora
+    public override void Centrifugar()
+    {
+        Console.WriteLine("La lavadora esta centrifugando");
+    }
+
+}
+public class Lavadora_Secadora : LavadoraBase
+{
+    public string TipoLavado{ get; set; }
+
+    public Lavadora_Secadora(string marca, string modelo, int año, string tipolavado)
+        : base(marca, modelo, año)
+    {
+        TipoLavado = tipolavado;
+    }
+
+    // Métodos específicos para la lavadora secadora
+    public override void Centrifugar()
+    {
+        Console.WriteLine("La lavadora esta secando.");
+    }
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ILavadora milavadora = new Lavadora("Samsung", "Inox", 2021, 4);
+        milavadora.Centrifugar();
+        milavadora.Lavar();
+        milavadora.Enjuagar();
+        milavadora.Remojar();
+
+        ILavadora milavadora_Secadora = new Lavadora_Secadora("Kalley", "K.ls14", 2022, "cara");
+        milavadora_Secadora.Centrifugar();
+        milavadora_Secadora.Lavar();
+        milavadora_Secadora.Enjuagar();
+        milavadora_Secadora.Remojar();
+    }
+}
---
+++ polimorfismo de williansteban/polimorfismo de williansteban/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace PolimorfismoYEncapsulamientowillian
+{
+    // Clase base
+    public abstract class Animal
+    {
+        // Propiedad encapsulada
+        private string nombre;
+
+        // Constructor
+        public Animal(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        // Método protegido para obtener el nombre (encapsulamiento)
+        protected string Nombre()
+        {
+            return nombre;
+        }
+
+        // Método abstracto que debe ser implementado por las clases derivadas (polimorfismo)
+        public abstract void Ladrar();
+    }
+
+    // Clase derivada
+    public class Perro : Animal
+    {
+        public Perro(string nombre) : base(nombre)
+        {
+        }
+
+        // método abstracto
+        public override void Ladrar()
+        {
+            Console.WriteLine($"{Nombre()} dice: Guau");
+        }
+    }
+
+    // Otra clase derivada
+    public class Gato : Animal
+    {
+        public Gato(string nombre) : base(nombre)
+        {
+        }
+
+        // método abstracto
+        public override void Ladrar()
+        {
+            Console.WriteLine($"{Nombre()} dice: ¡Miau!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creando instancias de Perro y Gato
+            Animal miPerro = new Perro("Rex");
+            Animal miGato = new Gato("Mittens");
+
+            // Usando polimorfismo para hacer que cada animal haga sonido
+            miPerro.Ladrar();
+            miGato.Ladrar();
+        }
+    }
+}
