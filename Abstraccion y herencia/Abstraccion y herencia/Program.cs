75b0599764aaff040824e9989900205bf0497f3f
Willian Gonzalez <willianstebangonzalezcortes@gmail.com>
2024-08-16 09:00:55 -0500

    se agregaron los dos ultimos ejercicios

---
+++ Abstraccion y herencia/Abstraccion y herencia/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// Clase abstracta Figura
+public abstract class Figura
+{
+    // Método abstracto para calcular el área
+    public abstract double CalcularArea();
+}
+
+// Clase Circulo que hereda de Figura
+public class Circulo : Figura
+{
+    public double Radio { get; set; }
+
+    public Circulo(double radio)
+    {
+        Radio = radio;
+    }
+
+    // Implementación del método abstracto
+    public override double CalcularArea()
+    {
+        return Math.PI * Radio * Radio; // Área de un círculo: π * r^2
+    }
+}
+
+// Clase Rectangulo que hereda de Figura
+public class Rectangulo : Figura
+{
+    public double Ancho { get; set; }
+    public double Alto { get; set; }
+
+    public Rectangulo(double ancho, double alto)
+    {
+        Ancho = ancho;
+        Alto = alto;
+    }
+
+    // Implementación del método abstracto
+    public override double CalcularArea()
+    {
+        return Ancho * Alto; // Área de un rectángulo: ancho * alto
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear una instancia de Circulo
+        Figura circulo = new Circulo(5); // Radio = 5
+        Console.WriteLine($"Área del círculo: {circulo.CalcularArea()}");
+
+        // Crear una instancia de Rectangulo
+        Figura rectangulo = new Rectangulo(4, 6); // Ancho = 4, Alto = 6
+        Console.WriteLine($"Área del rectángulo: {rectangulo.CalcularArea()}");
+    }
+}
---
+++ get and set willian steabn gonzalez/get and set willian steabn gonzalez/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Producto
+{
+    private decimal precio;
+
+    // Precio con get y set
+    public decimal Precio
+    {
+        get
+        {
+            return precio;
+        }
+        set
+        {
+            // el precio no puede ser negativo
+            if (value < 0)
+            {
+                Console.WriteLine("El precio no puede ser negativo.");
+            }
+            else
+            {
+                precio = value;
+            }
+        }
+    }
+
+    public Producto(decimal precioInicial)
+    {
+        Precio = precioInicial;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // precio inicial válido
+        Producto producto = new Producto(100m);
+
+        // precio actual
+        Console.WriteLine($"Precio del producto: {producto.Precio}");
+
+        // Intentamos establecer un precio negativo
+        producto.Precio = -50;
+
+        // el precio después de intentar asignar un valor negativo
+        Console.WriteLine($"Precio del producto: {producto.Precio}");
+
+        //precio válido
+        producto.Precio = 120m;
+
+        // precio actualizado
+        Console.WriteLine($"Precio del producto: {producto.Precio}");
+    }
+}
+
